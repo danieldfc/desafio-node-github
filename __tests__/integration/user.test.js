4decb99bb3406f3e58be266b5b7c473fda739cf5
Daniel Felizardo <daniel.david772@gmail.com>
2019-08-29 15:09:04 -0300

    Initial commit test

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,12 @@
+import request from 'supertest';
+import app from '../../src/app';
+
+import truncate from '../util/truncate';
+
+describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should be able to register', async () => {});
+});
