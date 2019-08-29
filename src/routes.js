a130c28a94b107f342f018c663326effb2032e2f
Daniel Felizardo <daniel.david772@gmail.com>
2019-08-29 15:08:24 -0300

    Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users/:id', UserController.update);
+
+export default routes;
